e21f6935698a76a3607afe7b8628eb38be7200f0
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-12 07:24:39 +0500

    Delegate:Add,Multiple Implement

---
+++ Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    delegate int Operation(int a, int b);
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    static int Multiple(int a, int b)
+    {
+        return a * b;
+    }
+    private static void Main(string[] args)
+    {
+        Operation operationAdd = Add;
+        Console.WriteLine(operationAdd(7, 9));
+
+        Operation operationMultiple = Multiple;
+        Console.WriteLine(operationMultiple(8, 4));
+    }
+}

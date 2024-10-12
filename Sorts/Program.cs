343d3f4ab27c3ca08c6d8a330ff86cb06ed0d20e
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-12 07:17:36 +0500

    Add project files.

---
+++ Sorts/Program.cs
@@ -0,0 +1,26 @@
+﻿//Buble Sort
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] Sonlar = { 1, 6, 0, -3, -4, 5, 0, 7 };
+
+        for (int i = 0; i < Sonlar.Length - 1; i++)
+        {
+            for (int j = 0; j < Sonlar.Length - 1; j++)
+            {
+                //Kamayish
+                if (Sonlar[j] < Sonlar[j + 1])
+                {
+                    int num = Sonlar[j];
+                    Sonlar[j] = Sonlar[j + 1];
+                    Sonlar[j + 1] = num;
+                }
+            }
+        }
+        for (int i = 0; i < Sonlar.Length; i++)
+        {
+            Console.WriteLine(Sonlar[i]);
+        }
+    }
+}
